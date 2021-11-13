bf82f4ac7413bc2ad4cb1ebe87b3bd0cd76f47eb
CandraJengger <cakjengger@gmail.com>
2021-11-13 20:48:14 +0700

    Remove Image

--- components/moleculs/CardMenu/index.tsx
+++ components/moleculs/CardMenu/index.tsx
@@ -6,16 +6,6 @@ import Image from 'next/image';
 const CardMenu: React.FC<Props> = ({ title, menus = [], price }) => {
   return (
     <div className="max-w-sm rounded-3xl shadow-xl inline-block mx-4 my-9 bg-white">
-      <div className="h-24 w-full mb-5">
-        <Image
-          src="https://source.unsplash.com/384x160"
-          height={160}
-          width={384}
-          layout="responsive"
-          objectFit="cover"
-          className="rounded-t-3xl"
-        />
-      </div>
       <div className="p-6 flex items-center flex-col">
         <Title as="h3" text={title || 'Paket 1'} />
         <div>
